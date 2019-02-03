ca3eefa15fe155dec1e96f1dc8663ffed52067d7
Ming Duan <11203duan@gmail.com>
2019-02-03 01:44:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(a)
   a.to_a
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(a)
+  a.capitalize
 end
 
 def find_the_cheese# code an argument here

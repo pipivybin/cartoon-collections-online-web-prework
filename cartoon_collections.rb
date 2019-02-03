8ed5020ea766e2d9fbd45f061d24d0ebe24c90f7
Ming Duan <11203duan@gmail.com>
2019-02-03 01:44:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def long_planeteer_calls(a)
   a.capitalize
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(a)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
